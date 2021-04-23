3d686567c4d1e06451f8b74d31ff699c96862e11
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-23 13:04:23 -0400

    Coding Exercise 1: Speed Converter

---
+++ CodingExercises/src/com/paulinavelazquez/section4exercises/SpeedConverter.java
@@ -0,0 +1,33 @@
+package com.paulinavelazquez.section4exercises;
+
+
+public class SpeedConverter {
+    public static void main(String[] args) {
+
+        printConversion(10.5);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        long result = 0;
+        if (kilometersPerHour < 0) {
+            result = -1;
+        } else {
+            double miles = kilometersPerHour / 1.609;
+            long roundedMiles = Math.round(miles);
+            result = roundedMiles;
+        }
+        return result;
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value.");
+        } else {
+            long miles = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + miles + " mi/h.");
+        }
+    }
+}
+
+
