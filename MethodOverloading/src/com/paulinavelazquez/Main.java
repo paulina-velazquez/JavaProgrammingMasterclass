6f6d482069e5adde76c0c2ac2b889267d9a08f50
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-23 13:35:57 -0400

    add method overloading

---
+++ MethodOverloading/src/com/paulinavelazquez/Main.java
@@ -0,0 +1,26 @@
+package com.paulinavelazquez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Carli", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+}
