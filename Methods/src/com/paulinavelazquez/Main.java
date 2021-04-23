d4a5975cc6d01c295cb8b76609e075f99036938b
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-23 10:51:14 -0400

    initial

---
+++ Methods/src/com/paulinavelazquez/Main.java
@@ -0,0 +1,29 @@
+package com.paulinavelazquez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int result = -1;
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was: " + finalScore);
+            result = finalScore;
+        }
+        return result;
+    }
+}
+
