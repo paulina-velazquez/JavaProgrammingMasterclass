56f9c9d68a8878500f85c676cff5b185391d2ac9
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-26 19:39:11 -0400

    Completed Digit Sum Challenge

---
+++ DigitSumChallenge/src/com/paulinavelazquez/Main.java
@@ -0,0 +1,25 @@
+package com.paulinavelazquez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(-125));
+        System.out.println(sumDigits(4));
+        System.out.println(sumDigits(32123));
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
