7d8f735c83893a25cd6d98308a43fd60190cab94
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-28 15:18:50 -0400

    Parsing Values from a String

---
+++ ParsingValuesFromString/src/com/paulinavelazquez/Main.java
@@ -0,0 +1,18 @@
+package com.paulinavelazquez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018.125";
+        System.out.println("numberAsString = " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+        System.out.println("Number " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("number asString = " + numberAsString);
+        System.out.println("Number = " + number);
+    }
+}
