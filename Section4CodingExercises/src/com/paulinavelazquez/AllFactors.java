8dbcb2c15bdc99e7a33f547cc44f595899c41022
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-28 09:56:31 -0400

    Completed Coding Exercise 21: All Factors

---
+++ Section4CodingExercises/src/com/paulinavelazquez/AllFactors.java
@@ -0,0 +1,23 @@
+package com.paulinavelazquez;
+
+public class AllFactors {
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+    public static void printFactors(int number ) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+        for (int i = 1; i < number + 1; i++) {
+            if (number % i == 0) {
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("");
+    }
+}
