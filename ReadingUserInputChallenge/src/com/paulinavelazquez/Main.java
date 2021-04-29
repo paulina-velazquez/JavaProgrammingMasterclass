df4234f95007edc38b171cef8d9afc00d19eae8f
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-29 11:01:56 -0400

    Completed Reading User Input Challenge

---
+++ ReadingUserInputChallenge/src/com/paulinavelazquez/Main.java
@@ -0,0 +1,26 @@
+package com.paulinavelazquez;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1;
+        int sum = 0;
+        while (counter <= 10) {
+            System.out.print("Enter number " + counter + "#: ");
+            boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                int userInput = scanner.nextInt();
+                sum += userInput;
+                counter++;
+            } else {
+                System.out.println("Invalid counter");
+            }
+            scanner.nextLine(); // handle end of line (enter key)
+        }
+        System.out.println("Sum = " + sum);
+        scanner.close();
+    }
+}
