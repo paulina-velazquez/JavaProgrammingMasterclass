636a03160c9f3ce41349e6248deab0a8ae77ff7d
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-04-29 16:31:57 -0400

    completed Constructor Challenge

--- ConstructorsChallenge/src/com/paulinavelazquez/Main.java
+++ ConstructorsChallenge/src/com/paulinavelazquez/Main.java
@@ -44,5 +44,44 @@ public class Main {
         BankAccount allisonsAccount = new BankAccount("Allison", "allison@domain", "12345");
         System.out.println(allisonsAccount.getAccountNumber() + " name " + allisonsAccount.getCustomerName());
 
+
+        System.out.println("\nVipCustomer Class");
+        VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+
+        VipCustomer person2 = new VipCustomer("Mary", 25000.00);
+        System.out.println(person2.getName());
+
+        VipCustomer person3 = new VipCustomer("Miranda", 1000.00, "miranda@domain.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getEmail());
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
