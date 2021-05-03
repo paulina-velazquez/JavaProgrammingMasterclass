9914b897c275f5f37ba14888e0d7a21c3c06f258
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-03 17:41:06 -0400

    completed Inheritance class

--- OOP - Classes, Objects, Constructions/src/com/paulinavelazquez/exercise33/carpetcostcalculator/Main.java
+++ OOP - Classes, Objects, Constructions/src/com/paulinavelazquez/exercise33/carpetcostcalculator/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        com.paulinavelazquez.carpetcostcalculator.Carpet carpet = new com.paulinavelazquez.carpetcostcalculator.Carpet(3.5);
+        Carpet carpet = new Carpet(3.5);
         Floor floor = new Floor(2.75, 4.0);
-        com.paulinavelazquez.carpetcostcalculator.Calculator calculator = new com.paulinavelazquez.carpetcostcalculator.Calculator(floor, carpet);
+        Calculator calculator = new Calculator(floor, carpet);
         System.out.println("total = " + calculator.getTotalCost());
 
-        carpet = new com.paulinavelazquez.carpetcostcalculator.Carpet(1.5);
+        carpet = new Carpet(1.5);
         floor = new Floor(5.4, 4.5);
-        calculator = new com.paulinavelazquez.carpetcostcalculator.Calculator(floor, carpet);
+        calculator = new Calculator(floor, carpet);
         System.out.println("total = " + calculator.getTotalCost());
     }
 }
