32e81d448460bdd3d66c7d74429974dba1b26688
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-06 11:45:37 -0400

    Completed Encapsulation class

---
+++ OOP - Composition, Encapsulation, and Polymorphism/src/com/paulinavelazquez/encapsulation/example/EnhancedPlayer.java
@@ -0,0 +1,24 @@
+package com.paulinavelazquez.encapsulation.example;
+
+public class EnhancedPlayer {
+
+    private String name;
+    private int hitPoints = 100;
+    private String weapon;
+
+    public EnhancedPlayer(String name, int hitPoints, String weapon) {
+        this.name = name;
+        if ((hitPoints > 0) && (hitPoints <= 100)) {
+            this.hitPoints = hitPoints;
+        }
+        this.weapon = weapon;
+    }
+
+    public int healthRemaining() {
+        return this.hitPoints;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+}
---
+++ OOP - Composition, Encapsulation, and Polymorphism/src/com/paulinavelazquez/encapsulation/example/Main.java
@@ -0,0 +1,20 @@
+package com.paulinavelazquez.encapsulation.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        player.name = "Victoria";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 21;
+        player.health = 100;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Amanda", 200, "Machete");
+        System.out.println("Initial health is " + enhancedPlayer.getHitPoints());
+    }
+}
