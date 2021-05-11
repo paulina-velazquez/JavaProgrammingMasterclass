ad6d9fcd6b0a263ae6d8e048184c9f4104fe348a
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-11 11:03:42 -0400

    Completed Arrays Challenge

--- Arrays, Lists, Autoboxing and Unboxing/src/com/paulinavelazquez/challenge100/arrays/Main.java
+++ Arrays, Lists, Autoboxing and Unboxing/src/com/paulinavelazquez/challenge100/arrays/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         int[] myIntegers = getIntegers(5);
         int[] sorted = sortIntegers(myIntegers);
         printArray(sorted);
