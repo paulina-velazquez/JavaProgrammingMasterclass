9dbbdbff0b0738f6e3305dddef3d5a996f61007d
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-11 13:43:15 -0400

    Completed Challenge 100: Arrays

---
+++ Arrays, Lists, Autoboxing, and Unboxing/src/com/paulinavelazquez/challenge100/arrays/Main.java
@@ -0,0 +1,42 @@
+package com.paulinavelazquez.challenge100.arrays;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] intArray = new int[capacity];
+        System.out.println("Enter 5 integer values.");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+        return intArray;
+    }
+
+    public static int[] sortIntegers(int[] intArray) {
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = i + 1; j < intArray.length; j++) {
+                if (intArray[i] < intArray[j]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp;
+                }
+            }
+        }
+        return intArray;
+    }
+
+    public static void printArray(int[] intArray) {
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("Element " + i + " contents " + intArray[i]);
+        }
+    }
+}
